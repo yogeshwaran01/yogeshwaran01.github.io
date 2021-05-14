d463b4815cf28c330149106956ae9890c889db9c
YOGESHWARAN R <yogeshin247@gmail.com>
2021-05-14 21:51:37 +0530

    Added Github Stats

---
+++ stats.js
@@ -0,0 +1,18 @@
+const params = new URLSearchParams(document.location.search);
+
+var username = location.pathname.split('/')[2]
+var _id = params.get("theme");
+var url = `https://github-stats-terminal-style.herokuapp.com/${username}?theme=${_id}`
+var content = "0; URL=" + url;
+
+var meta = document.createElement("meta");
+meta.setAttribute("http-equiv", "refresh");
+meta.setAttribute("content", content);
+
+document.getElementsByTagName("head")[0].appendChild(meta);
+
+var link = document.createElement("link");
+link.setAttribute("rel", "cononical");
+link.setAttribute("href", url);
+
+document.getElementsByTagName("head")[0].appendChild(link);
